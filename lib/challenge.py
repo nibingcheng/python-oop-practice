d08e9f543ac638d559a83c8c9daaed51f4f31627
Bingcheng Ni <nibingcheng@johndeere.com>
2021-03-05 20:00:35 -0600

    Prompts 1 to 5

--- lib/challenge.py
+++ lib/challenge.py
@@ -24,6 +24,21 @@
 #
 # Once you create your class definition create two instances.
 #
+# class Car:
+#     def __init__(self, make, model, color):
+#         self.make = make
+#         self.model = model
+#         self.color = color
+
+#     def drive(self):
+#         print('vroom vroom')
+
+# first_car = Car('Ford', 'Taurus', 'Black')
+# second_car = Car('Honda', 'Civic', 'Gray')
+
+# print(first_car.color)
+# second_car.drive()
+
 
 #
 # Prompt 2: We Do
@@ -36,7 +51,21 @@
 #
 # Make an instance of your Toyota class.
 #
+# class Toyota(Car):
+#     def __init__(self, model, color, make='Toyota'):
+#         super().__init__(make, model, color)
+#         # self.make = 'Toyota'
+#         # self.model = model
+#         # self.color = color
+        
+#     def __str__(self):
+#         return f'{self.make} {self.model} {self.color}'
 
+# third_car = Toyota('RAV4', 'White')
+# # print(third_car)
+# # third_car.drive()
+    
+    
 #
 # Prompt 3: You Do
 #
@@ -45,6 +74,30 @@
 #
 # After you've defined your class, create 3 instances.
 #
+# class Lion:
+#     def __init__(self, sex, size, color):
+#         self.sex = sex
+#         self.size = size
+#         self.color = color
+
+#     def hunt(self):
+#         print('Lion can kill')
+        
+#     def eat(self):
+#         print('Lion eats a lot')
+        
+#     def __str__(self):
+#         return f'{self.sex} {self.size} {self.color}'
+        
+# lion1 = Lion('female', 'small', 'brown')
+# lion2 = Lion('male', 'large', 'gray')
+# lion3 = Lion('female', 'mid-size', 'black')
+
+# print(lion1, '\n', lion2, '\n', lion3)
+# lion2.hunt()
+# lion3.eat()
+
+
 
 # Prompt 4: You Do
 #
@@ -56,6 +109,30 @@
 #   - add a property to your Toyota class
 #   - add a property to your Car class and "fill it in" for your Toyota class
 #
+class Car:
+    def __init__(self, make, model, style):
+        self.make = make
+        self.model = model
+        self.style = style
+        
+    def drive(self):
+        print('vroom vroom')
+
+class Toyota(Car):
+    def __init__(self, model, style, color, trans, make='Toyota'):
+        super().__init__(make, model, style)
+
+        self.trans = trans
+        self.color = color
+        
+    def __str__(self):
+        return f'{self.make} {self.model} {self.color} {self.style} {self.trans}'
+    
+
+new_car = Toyota('Camry', 'sedan', 'red', 'manual')
+# print(new_car)
+# new_car.drive()
+
 
 #
 # Prompt 5: You Do
@@ -72,6 +149,31 @@
 # Create an instance of your animal class (the one that extends the Animal
 # class).
 #
+class Animal:
+    def __init__(self, group):
+        self.group = group
+       
+    def sleep(self):
+        print('zzzzz')
+        
+    def eat(self):
+        print('yum yum')
+        
+    def __str__(self):
+        return f'{self.group}'
+
+class Lion(Animal):
+    def __init__(self, group='Mammals'):
+        super().__init__(group)
+        
+    def __str__(self):
+        return f'{self.group}'
+    
+new_lion = Lion()    
+# print(new_lion)
+# new_lion.eat()
+# new_lion.sleep()    
+
 
 #
 # Prompt 6: You Do
